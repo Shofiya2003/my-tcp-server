684795e289ddd4e7156deaac9c450755c87f1d0b
shofiya <shofiyabootwala@gmail.com>
2025-02-22 22:57:15 +0530

    feat: made the tcp server handle concurrent requests using go routines

--- main.go
+++ main.go
@@ -2,17 +2,18 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net"
-	"os"
+	"time"
 )
 
 func main() {
 	server, err := net.Listen("tcp", ":8080")
 	if err != nil {
-		fmt.Println("Error starting the server: ", err)
-		os.Exit(1)
+		log.Fatal("Error starting the server: ", err)
 	}
 	for {
+		// Accept() is a blocking call
 		conn, err := server.Accept()
 		if err != nil {
 			fmt.Println("Error accepting connections")
@@ -27,4 +28,18 @@ func main() {
 
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
+	fmt.Println("New Client Connected", conn.RemoteAddr().String())
+	// buffer := make([]byte, 1024)
+
+	// n, err := conn.Read(buffer)
+	// if err != nil {
+	// 	log.Println("Error reading from the ", err)
+	// }
+	// log.Println(buffer[:n])
+	time.Sleep(2 * time.Second)
+	_, err := conn.Write([]byte("Welcome!\n"))
+	if err != nil {
+		log.Println("Error writing to the client", err)
+	}
+
 }
